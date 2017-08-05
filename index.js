89eb4cc70d83816c54168d3d7472260e8c5d3973
sami9290 <sami9290@gmail.com>
2017-08-05 09:43:11 +0200

    vv

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var sFileNamesSEP  = '#' ;           // const
 var sClientDataSEP = '+' ;           // const
 var SERVER_URL='http://localhost/api';  //Url constant
 var dir="dir";                          //directory of saved latex files
-
+ssd
 // --- Common Variables ---
 var onlineUserDetails = {user_ID:0, file_toEdit:"file", activeEntry:false };
 var clientsNOW    = [] ;
